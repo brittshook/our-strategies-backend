3a06c478a40bc74aa474969ec42425da9ab34d59
Britt Shook <brittanysshook@gmail.com>
2024-02-06 16:39:21 -0500

    convert program delete route

--- routes/programs.js
+++ routes/programs.js
@@ -41,119 +41,24 @@ router
     }
   });
 
-// router.delete("/programs/:programId/?", (req, res, next) => {
-//   const programIndex = programs.findIndex(
-//     (program) => program.id == req.params.programId
-//   );
+router.delete("/programs/:id/?", async (req, res, next) => {
+  try {
+    const programId = req.params.id;
 
-//   if (programIndex != -1) {
-//     programs.splice(programIndex, 1);
-//     res.status(204).end();
-//   } else {
-//     next(error(404, "Program not found"));
-//   }
-// });
+    if (!programId) {
+      throw error(400, "Insufficient data");
+    }
 
-// router
-//   .route("/shifts/?")
-//   .get((req, res, next) => {
-//     res.json({ shifts: shifts });
-//   })
-//   .post((req, res, next) => {
-//     const name = req.body.name;
-//     const program = req.body.program;
-//     const startTime = req.body.startTime;
-//     const endTime = req.body.endTime;
-//     const location = req.body.location;
-//     const volunteerLimit = req.body.volunteerLimit;
+    const result = await Program.findByIdAndDelete(programId);
 
-//     if (name && program && startTime && endTime && volunteerLimit) {
-//       if (
-//         shifts.find(
-//           (shift) =>
-//             shift.name == name &&
-//             shift.program == program &&
-//             shift.startTime == startTime &&
-//             shift.endTime == endTime &&
-//             shift.location == location
-//         )
-//       ) {
-//         next(error(409, "Shift already exists"));
-//       }
+    if (result) {
+      res.status(204);
+    } else {
+      throw error(404, "Program not found");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
-//       const shift = {
-//         name: name,
-//         program: program,
-//         startTime: startTime,
-//         endTime: endTime,
-//         location: location,
-//         volunteerLimit: volunteerLimit,
-//       };
-
-//       shifts.push(shift);
-//       res.json({ shift: shift });
-//     } else {
-//       next(error(400, "Insufficient data"));
-//     }
-//   });
-
-// router
-//   .route("/shifts/:shiftId/?")
-//   .get((req, res, next) => {
-//     const shiftId = req.params.shiftId;
-//     const shift = shifts.find((shift) => shift.id == shiftId);
-
-//     if (shift) {
-//       res.json({ shift: shift });
-//     } else next(error(404, "Shift not found"));
-//   })
-//   .put((req, res, next) => {
-//     const shiftId = req.params.shiftId;
-//     const existingShift = shifts.find((shift) => shift.id == shiftId);
-
-//     if (existingShift) {
-//       const name = req.body.name;
-//       const program = req.body.program;
-//       const startTime = req.body.startTime;
-//       const endTime = req.body.endTime;
-//       const location = req.body.location;
-//       const volunteerLimit = req.body.volunteerLimit;
-
-//       if (
-//         name &&
-//         program &&
-//         startTime &&
-//         endTime &&
-//         location &&
-//         volunteerLimit
-//       ) {
-//         const shift = {
-//           name: name,
-//           program: program,
-//           startTime: startTime,
-//           endTime: endTime,
-//           location: location,
-//           volunteerLimit: volunteerLimit,
-//         };
-
-//         shifts.shiftId = shift;
-
-//         res.json({ shift: shift });
-//       } else {
-//         next(error(400, "Insufficient data"));
-//       }
-//     } else {
-//       next(error(404, "Shift not found"));
-//     }
-//   })
-//   .delete((req, res, next) => {
-//     const id =
-
-//     if () {
-
-//     } else {
-//       next(error(404, "Shift not found"));
-//     }
-//   });
-
-// module.exports = router;
+module.exports = router;
