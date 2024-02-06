5e2270b7d18f85b320688b92d0271c9e1ecd6329
Britt Shook <brittanysshook@gmail.com>
2024-02-06 16:15:43 -0500

    add user shift routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/user.js");
 const Shift = require("../models/shift.js");
+const ShiftAssignment = require("../models/shiftAssignment.js");
 const error = require("../utils/error.js");
 
 const router = express.Router();
@@ -9,8 +10,7 @@ router
   .route("/")
   .get(async (req, res, next) => {
     try {
-      const status = req.query.status;
-      const role = req.query.role;
+      const { status, role } = req.query;
       let query = {};
 
       if (status) {
@@ -110,7 +110,7 @@ router
       const result = await User.findByIdAndDelete(userId);
 
       if (result) {
-        res.status(204).json();
+        res.status(204);
       } else {
         next(error(404, "User not found"));
       }
@@ -119,76 +119,76 @@ router
     }
   });
 
-// router
-//   .route("/:userId/shifts/?")
-//   .get((req, res, next) => {
-//     const userId = req.params.userId;
-//     const startTime = req.query.startTime;
-//     const endTime = req.query.endTime;
-//     const user = users.find((user) => user.id == userId);
+router
+  .route("/:id/shifts/?")
+  .get(async (req, res, next) => {
+    try {
+      const userId = req.params.id;
 
-//     if (user) {
-//       if (user.shifts) {
-//         if (startTime && endTime) {
-//           const filteredShifts = user.shifts.filter((shift) => {
-//             return shift.startTime >= startTime && shift.endTime <= endTime;
-//           });
-//           res.json({ shifts: filteredShifts });
-//         } else if (startTime || endTime) {
-//           next(
-//             error(
-//               400,
-//               "To filter by time range, both start time and end time must be provided"
-//             )
-//           );
-//         } else {
-//           res.json({ shifts: user.shifts });
-//         }
-//       } else {
-//         next(error(404, "No shifts found"));
-//       }
-//     } else {
-//       next();
-//     }
-//   })
-//   .post((req, res, next) => {
-//     const userId = req.params.userId;
-//     const user = users.find((user) => user.id == userId);
+      if (!userId) {
+        next(error(400, "Insufficient data"));
+      }
 
-//     if (user) {
-//       const shiftId = req.body.shiftId;
-//       const shift = shifts.find((shift) => shift.id == shiftId);
+      const { startTime, endTime } = req.query;
+      const query = { userId };
 
-//       if (shift) {
-//         if (!user.hasOwnProperty("shifts")) {
-//           user.shifts = [];
-//         }
-//         user.shifts.push(shift);
-//         res.json({ shifts: user.shifts });
-//       } else {
-//         next(error(404, "Shift not found"));
-//       }
-//     } else {
-//       next(error(404, "User not found"));
-//     }
-//   });
+      if (startTime) {
+        query.startTime = { $gte: startTime };
+      }
 
-// router.delete("/:userId/shifts/:shiftId/?", (req, res, next) => {
-//   const userId = req.params.userId;
-//   const user = users.find((user) => user.id == userId);
+      if (endTime) {
+        query.endTime = { $lte: endTime };
+      }
 
-//   if (user) {
-//     const shiftIndex = user.shifts.findIndex(
-//       (shift) => shift.id == req.params.shiftId
-//     );
+      const result = await ShiftAssignment.find(query)
+        .populate("shiftId")
+        .limit(1000);
 
-//     if (shiftIndex != -1) {
-//       user.shifts.splice(shiftIndex, 1);
-//       res.status(204).end();
-//     } else {
-//       next(error(404, "Shift not found"));
-//     }
-//   }
-// });
+      if (result) {
+        res.json({ shifts: shiftAssignments });
+      } else {
+        next(error(400, "User not found"));
+      }
+    } catch (err) {
+      next(err);
+    }
+  })
+  .post(async (req, res, next) => {
+    try {
+      const userId = req.params.id;
+      const shiftId = req.body.shiftId;
 
-// module.exports = router;
+      if (!userId || !shiftId) {
+        next(error(400, "Insufficient data"));
+      }
+
+      const result = await ShiftAssignment.create({ userId, shiftId });
+
+      if (result) {
+        res.status(201).json({ shiftAssignment: result });
+      } else {
+        next(error(404, "Shift not found"));
+      }
+    } catch (err) {
+      next(err);
+    }
+  });
+
+router.delete("/:id/shifts/:shiftId/?", async (req, res, next) => {
+  const userId = req.params.id;
+  const shiftId = req.params.shiftId;
+
+  if (!userId || !shiftId) {
+    next(error(400, "Insufficient data"));
+  }
+
+  const result = await ShiftAssignment.find({ userId, shiftId });
+
+  if (result) {
+    res.status(204);
+  } else {
+    next(error(404, "Shift not found"));
+  }
+});
+
+module.exports = router;
