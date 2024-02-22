342e5594b507bbcfd31faeb6b6a6661a4961f224
Britt Shook <brittanysshook@gmail.com>
2024-02-22 11:36:00 -0500

    add profle image field, set default

--- models/user.js
+++ models/user.js
@@ -8,6 +8,11 @@ const userSchema = new Schema(
     last: { type: String, required: true },
     email: { type: String, required: true },
     address: { type: String, required: true },
+    profileImageId: {
+      type: Schema.Types.ObjectId,
+      ref: "ProfileImage",
+      default: "65d77616fc9d9a2308bad38e",
+    },
     role: {
       type: [
         {
